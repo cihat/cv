d81370d88ec557e70a8f5d37fbfc27c4a946b8c7
Cihat SALİK <57585087+cihat@users.noreply.github.com>
2025-06-28 18:16:09 +0300

    Remove experimental CSS optimization from Next.js configuration and update SVG properties in WebsiteIcon component for consistency.

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    optimizeCss: true,
-  },
   compress: true,
   poweredByHeader: false,
   generateEtags: false,
